5f44bf6e9f612bfcf41936c50c7b6c9a7932a7d8
diegorodrigues12 <diegorodriguesoli9@gmail.com>
2026-05-23 20:08:34 -0300

    subindo config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGDvRBfJlztDRpQS1MHkYlDQst7ZtfHCo",
+  authDomain: "mundoria-53de5.firebaseapp.com",
+  projectId: "mundoria-53de5",
+  storageBucket: "mundoria-53de5.firebasestorage.app",
+  messagingSenderId: "86901345396",
+  appId: "1:86901345396:web:969fa3637292d0333e1521"
+};
